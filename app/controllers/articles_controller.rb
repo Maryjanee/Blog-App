4c93cf9c9ab6a0d8f133097c38722cd670e6a046
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-08-28 14:36:55 +0100

    Add Edit and Update methods and their respective views

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,4 +17,20 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+  
+  def destroy
+    @article = Article.find(params[:id]).destroy;
+    redirect_to articles_url
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' Updated!"
+    redirect_to article_path(@article)
+  end
 end
