2f34afbfd91bf4c12821d757d40e0cafcbcb609f
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2020-08-31 14:19:36 +0100

    add model for Tagging

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
+  
+  def tag_list
+    self.tags.collect|tag|
+    tag.name
+  end.join(", ")
+  end
+  
 end
